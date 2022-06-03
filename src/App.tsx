a01e44ffb62840a1b1a7854f518eb5ac45b45d4e
Ciahaka <wilddiver.by@gmail.com>
2022-06-03 13:21:58 +0300

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./Start/Header";
+import {Body} from "./Start/Body";
+import {Footer} from "./Start/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </>
+    )
+        ;
 }
 
 export default App;
