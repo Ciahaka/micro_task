6e4662dbdd5499bc86359bcd393db0ff5d70d1d8
Ciahaka <wilddiver.by@gmail.com>
2022-06-05 00:11:14 +0300

    commit2_Button

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,3 +1,20 @@
 import React from "react";
 import './Button.css'
 
+type ButtonType={
+  name:string
+  callBack:()=>void
+}
+
+export const Button = (props:ButtonType) => {
+  const onClickHandler=()=>{
+    props.callBack()
+  }
+  return (
+    <>
+      <button onClick={onClickHandler}>{props.name}</button>
+
+    </>
+  )
+}
+
