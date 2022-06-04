95bec0686cdf4db00b268e9c1e735532b6446dc1
Ciahaka <wilddiver.by@gmail.com>
2022-06-05 02:16:00 +0300

    addStateForBanknotes

---
+++ src/Banknots/stateForBanknots.tsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+const [money, setMoney] = useState([
+  { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+  { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+  { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+  { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+  { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+  { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+  { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+  { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+])
