c938a79ce70792f9ea37ca310349981243e5586a
Ciahaka <wilddiver.by@gmail.com>
2022-06-04 18:00:03 +0300

    Change_AppCars

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,48 @@ import {Header} from "./Start/Header";
 import {Body} from "./Start/Body";
 import {Footer} from "./Start/Footer";
 import {LessonMap} from "./Map/Comonente map";
+import {Cars} from "./TopCars/TopCars";
 
 
 function App() {
-    // return (
+     // return (
     //     <>
     //         <Header titleForHeader={'New Header'}/>
     //         <Body titleForBody = {'New Body'}/>
     //         <Footer titleForFooter = {'New Footer'}/>
-    //
-    //
-    //
+
     //     </>
     // );
-    let students = [
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100},
-
-    ];
-
-    return (
-        <LessonMap students={students}/>
-    )
-
+    //-------------------------------------------------------------------------------------
+    // let students = [
+    //     {id: 1, name: "James", age: 8},
+    //     {id: 2, name: "Robert", age: 18},
+    //     {id: 3, name: "John", age: 28},
+    //     {id: 4, name: "Michael", age: 38},
+    //     {id: 5, name: "William", age: 48},
+    //     {id: 6, name: "David", age: 58},
+    //     {id: 7, name: "Richard", age: 68},
+    //     {id: 8, name: "Joseph", age: 78},
+    //     {id: 9, name: "Thomas", age: 88},
+    //     {id: 10, name: "Charles", age: 98},
+    //     {id: 11, name: "Christopher", age: 100},
+    //
+    // ];
+    //
+    // return (
+    //     <LessonMap students={students}/>
+    // )
+//-----------------------------------------------------------------------------------------------------
     const topCars = [
         {manufacturer:"BMW",model:'m5cs'},
         {manufacturer:"Mercedes",model:'e63s'},
         {manufacturer:"Audi",model:'rs6'}
-    ]
+    ];
+    return (
+        <Cars topCars={topCars}/>
+    )
+
+
 }
-export default App;
-//
+ export default App;
+
