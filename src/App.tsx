1b0a906f1ec60393c3a0846bb4240d984471ddd3
Ciahaka <wilddiver.by@gmail.com>
2022-11-01 15:44:54 +0300

    change App

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,27 @@ import {Body} from './component/header_props/Body';
 import {Footer} from './component/header_props/Footer';
 import {Students} from './component/students/Students';
 import {TopCars} from './component/topCars/TopCars';
-import {Button, onClickHandler} from './component/button/Button';
-
+import {Button} from './component/button/Button';
 
 
 const topCars = [
-  {id:1, manufacturer:'BMW',model:'m5ce'},
-  {id:2,manufacturer:'Mercedes',model:'e63s'},
-  {id:3,manufacturer:'Audi',model:'rs6'},
+  {id: 1, manufacturer: 'BMW', model: 'm5ce'},
+  {id: 2, manufacturer: 'Mercedes', model: 'e63s'},
+  {id: 3, manufacturer: 'Audi', model: 'rs6'},
 ]
 
 const students = [
-  {id: 1, name: "James", age: 8},
-  {id: 2, name: "Robert", age: 18},
-  {id: 3, name: "John", age: 28},
-  {id: 4, name: "Michael", age: 38},
-  {id: 5, name: "William", age: 48},
-  {id: 6, name: "David", age: 58},
-  {id: 7, name: "Richard", age: 68},
-  {id: 8, name: "Joseph", age: 78},
-  {id: 9, name: "Thomas", age: 88},
-  {id: 10, name: "Charles", age: 98},
-  {id: 11, name: "Christopher", age: 100},
+  {id: 1, name: 'James', age: 8},
+  {id: 2, name: 'Robert', age: 18},
+  {id: 3, name: 'John', age: 28},
+  {id: 4, name: 'Michael', age: 38},
+  {id: 5, name: 'William', age: 48},
+  {id: 6, name: 'David', age: 58},
+  {id: 7, name: 'Richard', age: 68},
+  {id: 8, name: 'Joseph', age: 78},
+  {id: 9, name: 'Thomas', age: 88},
+  {id: 10, name: 'Charles', age: 98},
+  {id: 11, name: 'Christopher', age: 100},
 ]
 
 function App() {
