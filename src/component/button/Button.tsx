55a6f69d71423bd8d04e38289c4581e9a6eb60aa
Ciahaka <wilddiver.by@gmail.com>
2022-11-01 15:44:45 +0300

    change Button

--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, {MouseEvent} from 'react'
 import s from './Button.module.css'
 
 
-// export const newSubscriber = () => {
-//   alert('Сам ты Вася!')
-// }
-//
-// export const newSubscriber_2 = () => {
-//   alert('Сам ты Вася!')
-// }
-// export const admin = () => {
-//   alert('А ну, заткнулись оба!')
-// }
-
-
-
-export const onClickHandler =(phrase:string)=> {
-  console.log(phrase)
-}
-
 export const Button = () => {
 
+  const onClickHandler = (name: string) => {
+    alert(name)
+  }
+  // const newSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+  //   alert('Срочно нажал и подписался, Вася!')
+  // }
+  //
+  // const newSubscriber_2 = (event:MouseEvent<HTMLButtonElement>) => {
+  //   alert('Сам ты Вася!')
+  // }
+  // const admin = () => {
+  //   alert('А ну, заткнулись оба!')
+  // }
 
   return (
     <div className={s.site}>
-      <button className={s.button} onClick={(event)=>onClickHandler}>Mir Podvoha Youtube Chanel</button>
-      <button className={s.button_2} onClick={(event)=>onClickHandler}>Оветочка</button>
-      <button className={s.button_3} onClick={(event)=>onClickHandler}>Злой админ</button>
+      <button className={s.button} onClick={(event)=>onClickHandler('Срочно нажал и подписался, Вася!')}>Mir Podvoha</button>
+      <button className={s.button_2} onClick={(event)=>onClickHandler('Сам ты Вася!')}> Subscriber</button>
+      <button className={s.button_3} onClick={(event)=>onClickHandler('А ну, заткнулись оба!')}>Admin</button>
     </div>
+
   );
-};
+
+}
+
 
