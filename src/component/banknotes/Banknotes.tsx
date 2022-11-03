1df477d785acd59058f6ff7a32277005f954c911
Ciahaka <wilddiver.by@gmail.com>
2022-11-04 00:15:26 +0300

    change Banknotes

--- src/component/banknotes/Banknotes.tsx
+++ src/component/banknotes/Banknotes.tsx
@@ -1,21 +1,29 @@
 import React, {useState} from 'react';
-import s from './Banknotes.module.css'
 
-type FilterType='All'|'Dollars'|'Rubles'
+import {DzBanknotes, FilterType} from './dz banknotes/DzBanknotes';
+
+
 export const Banknotes = () => {
+
   const [money, setMoney] = useState([
-    { banknotes: 'Dollars', nominal: 100, number: ' a1234567890' },
-    { banknotes: 'Dollars', nominal: 50, number: ' z1234567890' },
-    { banknotes: 'Rubles', nominal: 100, number: ' w1234567890' },
-    { banknotes: 'Dollars', nominal: 100, number: ' e1234567890' },
-    { banknotes: 'Dollars', nominal: 50, number: ' c1234567890' },
-    { banknotes: 'Rubles', nominal: 100, number: ' r1234567890' },
-    { banknotes: 'Dollars', nominal: 50, number: ' x1234567890' },
-    { banknotes: 'Rubles', nominal: 50, number: ' v1234567890' },
+    {banknotes: 'Dollars', nominal: 100, number: ' a1234567890'},
+    {banknotes: 'Dollars', nominal: 50, number: ' z1234567890'},
+    {banknotes: 'Rubles', nominal: 100, number: ' w1234567890'},
+    {banknotes: 'Dollars', nominal: 100, number: ' e1234567890'},
+    {banknotes: 'Dollars', nominal: 50, number: ' c1234567890'},
+    {banknotes: 'Rubles', nominal: 100, number: ' r1234567890'},
+    {banknotes: 'Dollars', nominal: 50, number: ' x1234567890'},
+    {banknotes: 'Rubles', nominal: 50, number: ' v1234567890'},
   ])
-  const[filter,setFilter]=useState<FilterType>('All')
 
   let currentMoney = money;
+
+  const [filter, setFilter] = useState('All')
+
+  if (filter === 'all') {
+    currentMoney = money
+  }
+
   if (filter === 'Dollars') {
     currentMoney = money.filter(filteredMoney => filteredMoney.banknotes === 'Dollars');
   }
@@ -23,29 +31,28 @@ export const Banknotes = () => {
     currentMoney = money.filter(filteredMoney => filteredMoney.banknotes === 'Rubles');
   }
 
-  const onClickFilterHandler = (nameButton: FilterType) => {
+  let onClickFilterHandler = (nameButton: FilterType) => {
     setFilter(nameButton)
   }
 
-return (
-  <div className={s.site}>
-    <ul >
-      {currentMoney.map((objFromMoneyArr, index) => {
-        return (
-          <li key={index} className={s.li}>
-            <span className={s.span}> {`Банкнота ${objFromMoneyArr.banknotes} номиналом ${objFromMoneyArr.nominal} серийный номер ${objFromMoneyArr.number}`}</span>
-          </li>
-        )
-      })}
-    </ul>
-    <div>
-      <button className={s.all} onClick={() => onClickFilterHandler('All')}>all</button>
-      <button className={`${s.all} ${s.rub}`} onClick={() => onClickFilterHandler('Rubles')}>rubles</button>
-      <button className={`${s.all} ${s.usd}`} onClick={() => onClickFilterHandler("Dollars")}>dollars</button>
-    </div>
-  </div>
-)
-
-
+  return (
+    <DzBanknotes currentMoney={currentMoney} callback={onClickFilterHandler}/>
+    //  <div className={s.site}>
+    //    <ul >
+    //      {currentMoney.map((objFromMoneyArr, index) => {
+    //        return (
+    //          <li key={index} className={s.li}>
+    //            <span className={s.span}> {`Банкнота ${objFromMoneyArr.banknotes} номиналом ${objFromMoneyArr.nominal} серийный номер ${objFromMoneyArr.number}`}</span>
+    //          </li>
+    //        )
+    //      })}
+    //    </ul>
+    //    <div>
+    //      <button className={s.all} onClick={() => onClickFilterHandler('All')}>all</button>
+    //      <button className={`${s.all} ${s.rub}`} onClick={() => onClickFilterHandler('Rubles')}>rubles</button>
+    //      <button className={`${s.all} ${s.usd}`} onClick={() => onClickFilterHandler("Dollars")}>dollars</button>
+    //    </div>
+    //  </div>
+  )
 };
 
