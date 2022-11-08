259e245bad65af61f12192edf0a49dd2e8cf124c
Ciahaka <wilddiver.by@gmail.com>
2022-11-09 01:14:42 +0300

    change Input

--- src/component/universal input/input/Input.tsx
+++ src/component/universal input/input/Input.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+
 
 export const Input = () => {
+  let [text, setText] = useState('')
+
+  const eventInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.currentTarget.value)
+  }
+
   return (
     <div>
-
+      <input onChange={eventInputHandler}/>
     </div>
   );
 };
