1107271e6ea8e408e30c8b9db6a524a0e83429f3
Ciahaka <wilddiver.by@gmail.com>
2022-11-09 20:43:34 +0300

    change Input

--- src/component/universal input/input/Input.tsx
+++ src/component/universal input/input/Input.tsx
@@ -2,10 +2,10 @@ import React, {ChangeEvent, useState} from 'react';
 
 
 export const Input = () => {
-  let [text, setText] = useState('')
+  let [textInput, setInputText] = useState('')
 
   const eventInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setText(event.currentTarget.value)
+    setInputText(event.currentTarget.value)
   }
 
   return (
